398f43dedf0e1ab2ca0f54e9532af92b2c8cf70d
zhangyang <zjsxzy@gmail.com>
2014-11-22 23:26:50 +0800

    update

--- pcn_test.py
+++
@@ -1,6 +0,0 @@
-from numpy import *
-inputs = array([[0,0], [0,1], [1,0], [1,1]])
-targets = array([[0], [1], [1], [1]])
-import pcn
-p = pcn.pcn(inputs, targets)
-p.pcntrain(inputs, targets, 0.25, 6)
